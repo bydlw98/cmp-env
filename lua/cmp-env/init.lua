d05fee5cf17da5c06edefa67f34db408cd95fc7a
amarakon <amar.al-zubaidi45@tutanota.com>
2022-10-09 19:50:14 -0400

    Remove unnecessary function: `get_keyword_pattern`

--- lua/cmp-env/init.lua
+++ lua/cmp-env/init.lua
@@ -8,10 +8,6 @@ source.get_trigger_characters = function()
 	return { "$" }
 end
 
-source.get_keyword_pattern = function()
-	return "\\$[^[:blank:]]*"
-end
-
 source.complete = require("cmp-env.complete")
 
 return source
